409f696fcf0d1f1e82cc3929b3fdd9faa335e40b
Christine Nguyen <71248843+christinetn@users.noreply.github.com>
2023-07-03 17:33:29 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,403 @@
+
+
+import os, sys
+from bs4 import BeautifulSoup
+import json
+import re
+from nltk.stem import PorterStemmer
+import math
+
+'''
+Loops through the directories and files under DEV to read and parse the json files.
+Converts the json file into a dictionary and assigns the url a unique document ID.
+Reads the contents of the page and stems and tokenizes the document.
+The indexer is a dictionary with the key being the token and the value being
+another dictionary that holds the document ID as the key and the frequency of the token
+in that document as the value.
+
+Finds which html tags the tokens were in and adds the corresponding weights 
+as the html tag weights calculated in query.py
+
+Source for reading into directories: https://stackoverflow.com/questions/39508469/reading-all-the-file-content-in-directory
+Source for reading json into dictionary: https://www.geeksforgeeks.org/convert-json-to-dictionary-in-python/
+Source for finding all headers in html: https://stackoverflow.com/questions/45062534/how-to-grab-all-headers-from-a-website-using-beautifulsoup
+'''
+
+
+def run():
+    porterStem = PorterStemmer()
+    idCounter = 1
+    docID = {}
+    fileCount = 0  # counts number of files before writing to disk
+    totalFileCount = 0  # counts number of total files for tf-idf
+    bitIndexes = []  # holds all the bit indexes for all files we wrote to
+    validJsonCounter = 0  # valid json files
+    frequencies = {}  # token frequencies
+
+    assignmentFolder = os.getcwd() + '/DEV' #goes through dev folder, loops thru directories to go thru all docs
+    for directories in os.listdir(assignmentFolder):
+        subfolder = os.path.join(assignmentFolder, directories)
+        for file in os.listdir(os.path.join(assignmentFolder, directories)):
+
+            if (fileCount == 1000):  #cs/se constraint where we need to write index to file in batches 
+                fileCount = 0 #every 1k files indexed, write index into file, reset index 
+                ind = writeToFile(frequencies, len(bitIndexes))
+                bitIndexes.append(ind)
+                frequencies = {}
+
+            with open(os.path.join(subfolder, file), 'r') as j: #open file 
+                jsonContentDictionary = json.load(j)
+                currURL = ''
+
+                for key, value in jsonContentDictionary.items():#looking at the json file....
+                    if key == 'url': #if key is url store url and put it into dictionary of doc IDs (assigns url an ID)
+                        currURL = value
+                        validJsonCounter = validJsonCounter + 1
+                        docID[str(idCounter)] = currURL
+
+                    if key == 'content':#if key is content, where we do processing/indexing 
+                        soup = BeautifulSoup(value, 'html.parser') #html parser fixes broken html
+
+                        # find and calculate html tag weights (g(d)) 
+                        header = soup.find_all(re.compile('^h[1-6]$')) #regex to collect everything in between header tags
+                        headerText = [] #want only text and no other html tags 
+                        for h in header: #loop thru list of header text
+                            if len(h) > 0: #make sure its not empty
+                                text = re.findall(r'>(.*)<', str(h)) #use regex to find in between 
+                                for t in text:
+                                    t = t.replace('\n', '')#changed new line to space b/c realized that new lines between text gave errors
+                                    while t.find('<') != -1:  #fix nested html tags since regex goes thru 1 pair of text 
+                                        start = t.find('<') #loop goes thru start and end of html tags to rid of it 
+                                        end = t.find('>', start)
+                                        t = t[:start] + t[end + 1:]
+                                    if len(t) > 0:
+                                        headerText.append(t)
+
+                        del header
+                        headerTokens = [] #lines 78-83 tokenize each text and appends it to a list holding tokens that appear in headers 
+                        for t in headerText:
+                            hTokens = tokenizer(t)
+                            for t2 in hTokens:
+                                if len(t2) > 1:
+                                    headerTokens.append(t2)
+                        del headerText #rid of memory 
+
+#lines 87-95 find corresponding text of tags like for header..didntc call method for header b/c needed extra regex to find headers 1-6  
+                        titleTokens = findTags(soup, 'title')
+                        # finding bold text
+                        boldTokens = findTags(soup, 'b')
+                        # finding strong (bold) text
+                        strongTokens = findTags(soup, 'strong')
+                        # finding italic text
+                        italicTokens = findTags(soup, 'i')
+                        # finding emphasized (italic) text
+                        emphasizedTokens = findTags(soup, 'em')
+                        # finding small text
+                        # smallTokens = findTags(soup, 'small')
+                        # assume that text not a part of these are body text
+
+                        tokens = tokenizer(soup.text)  #tokenize the document
+
+                        for t in tokens:  #goes through list of tokens and stems the word 
+                            stemmedT = porterStem.stem(t) # used porter stemmer lib
+                            if stemmedT in frequencies.keys(): #create dict with token as key pointing to another dict with docID as key: list where [0] is term frequency of tf-ifd [1] is html tag weight 
+                                if str(idCounter) in frequencies[stemmedT].keys():
+                                    frequencies[stemmedT][str(idCounter)][0] += 1
+                                else:
+                                    frequencies[stemmedT][str(idCounter)] = [1, 0] #html tag weight initialized to 0
+                            else:
+                                frequencies[stemmedT] = {}
+                                frequencies[stemmedT][str(idCounter)] = [1, 0]
+
+                            # title = 1, headers = 0.85, bold/strong = .75, italic/emph = 0.5, other(small/body) = 0.1
+                            if t in titleTokens: #lines 114-133 where we initialze the html tag weights 
+                                frequencies[stemmedT][str(idCounter)][1] += 1
+                                titleTokens.remove(t)
+                            elif t in headerTokens:
+                                frequencies[stemmedT][str(idCounter)][1] += 0.85
+                                headerTokens.remove(t)
+                            elif t in boldTokens:
+                                frequencies[stemmedT][str(idCounter)][1] += 0.75
+                                boldTokens.remove(t)
+                            elif t in strongTokens:
+                                frequencies[stemmedT][str(idCounter)][1] += 0.75
+                                strongTokens.remove(t)
+                            elif t in italicTokens:
+                                frequencies[stemmedT][str(idCounter)][1] += 0.5
+                                italicTokens.remove(t)
+                            elif t in emphasizedTokens:
+                                frequencies[stemmedT][str(idCounter)][1] += 0.5
+                                emphasizedTokens.remove(t)
+                            else:
+                                frequencies[stemmedT][str(idCounter)][1] += 0.1
+
+                fileCount += 1 #increment for writing index to files
+                totalFileCount += 1 #incremeent for tf-ifd calculation
+                idCounter += 1 #for docID
+
+    if len(frequencies) != 0: #after looping through all the files, check if index has anything left ..
+        ind = writeToFile(frequencies, len(bitIndexes)) #write index into file again and append the pointer index tot he pointer index list
+        bitIndexes.append(ind)
+#We write the dictionary holding the urls and docIDs into file so we can use it in query.py
+    with open('docID.json', 'w+') as d:
+        json.dump(docID, d)
+        d.close()
+
+    mergeFiles(bitIndexes, totalFileCount) #call merge files, take all the files we created for index, merge 1 file, where we calc tf-idf for each doc under each token
+    lengthNormalization() #deals cosine similarity, normalizes tf-idf scores 
+    writeCacheWords() #to be under time constrainst , function writes cache words from index into file that query will look into
+
+    del frequencies
+    del fileCount
+
+    print('Done Indexing!')
+
+
+'''
+Tokenizes the contents of the files into alphanumeric tokens
+'''
+
+
+def tokenizer(contents): 
+    tokens = re.findall(r'[a-z0-9]+', contents.lower())
+    return tokens
+
+
+'''
+Finds all the html tags that mark the tokens as important
+Bold/strong, italics/emphasized, headers, title
+'''
+
+
+def findTags(soup, pattern):
+    tag = soup.find_all(pattern)
+    text = []
+    for i in tag:
+        if len(i) > 0:
+            i = str(i).replace('\n', '')
+            t = re.findall(r'>(.*)<', i)[0]
+            while t.find('<') != -1:
+                start = t.find('<')
+                end = t.find('>', start)
+                t = t[:start] + t[end + 1:]
+            if len(t) > 0:
+                text.append(t)
+    del tag
+    tokens = []
+    for t in text:
+        alphanumTokens = tokenizer(t)
+        for t2 in alphanumTokens:
+            if len(t2) > 1:
+                tokens.append(t2)
+    del text
+    return tokens
+
+
+'''
+Writes the current index into a file
+'''
+
+
+def writeToFile(index, writeCount):
+    fileName = 'cs121-disk' + str(writeCount) + '.json'
+    bitIndex = {}
+    index = dict(sorted(index.items()))
+
+    with open(fileName, 'w+') as f:
+        currBit = 0
+        for k, v in index.items():
+            bitIndex[k] = currBit
+            temp = {k: v}
+            json.dump(temp, f)
+            f.write('\n')
+            currBit += len(str(temp)) + 2
+
+        f.close()
+
+    return bitIndex
+
+
+'''
+Merges all files together to build a single file with the complete index.
+Writes all files into a single file first with duplicates and then
+creates the final file that merges those entries together.
+'''
+
+
+# https://stackoverflow.com/questions/7945182/opening-multiple-an-unspecified-number-of-files-at-once-and-ensuring-they-are
+def mergeFiles(bitIndexes, totalFileCount):
+    openFiles = []
+    files = []
+    bitIndex = {}
+    finalIndex = {}
+
+    for i in range(len(bitIndexes)):
+        files.append('cs121-disk' + str(i) + '.json')
+    try:
+        for f in files:
+            openFiles.append(open(f, 'r'))
+
+        # merges all files into 1 file with duplicate tokens
+        # makes the file pointer index hold list of pointers to
+        # the duplicate entries to merge later
+        with open('cs121-disk-duplicates.json', 'w+') as c:
+            currBit = 0
+            for f, b in zip(openFiles, bitIndexes):
+                for k, v in b.items():
+
+                    if k in bitIndex.keys():
+                        bitIndex[k].append(currBit)
+                    else:
+                        bitIndex[k] = [currBit]
+
+                    f.seek(v)
+                    line = f.readline()
+                    j = json.loads(line)
+                    json.dump(j, c)
+                    c.write('\n')
+                    t = currBit
+                    currBit += len(str(j)) + 2
+
+            c.close()
+
+        # writes into final file by merging entries in duplicates
+        with open('cs121-disk-final.json', 'w+') as final:
+            with open('cs121-disk-duplicates.json', 'r') as c:
+                currBit = 0
+                for k, v in bitIndex.items():
+                    finalIndex[k] = currBit
+                    if len(v) > 1:
+                        c.seek(v[0])
+                        t = json.loads(c.readline())
+                        tValue = list(t.values())[0]
+                        for i in v[1:]:
+                            c.seek(i)
+                            temp = list(json.loads(c.readline()).values())[0]
+                            tValue.update(temp)
+
+                        t[list(t.keys())[0]] = tValue
+                        t = calculateTF_IDF(t, totalFileCount)
+                        json.dump(t, final)
+
+                    else:
+                        c.seek(v[0])
+                        t = json.loads(c.readline())
+                        t = calculateTF_IDF(t, totalFileCount)
+                        json.dump(t, final)
+
+                    final.write('\n')
+                    currBit += len(str(t)) + 2
+
+                c.close()
+            final.close()
+
+    finally:
+        for f in openFiles:
+            f.close()
+
+    with open('final-index.json', 'w+') as f:
+        json.dump(finalIndex, f)
+        f.close()
+
+
+'''
+Calculates the TF_IDF for all the documents under the token
+'''
+
+
+def calculateTF_IDF(t, totalFileCount):
+    docuFreq = len(list(t.values())[0])
+    for k, v in t.items():
+        for doc, freq in v.items():
+            tf_idf = (1 + math.log(freq[0])) * math.log(totalFileCount / docuFreq)
+            t[k][doc][0] = tf_idf
+    return t
+
+
+'''
+Normalizes the TF-IDF by going through the index file and adding
+all the TF-IDF scores squared, then square roots and divides
+each of the TF-IDF scores in final file.
+'''
+
+
+def lengthNormalization():
+    normalizeBitLoc = {}
+    currBit = 0
+
+    with open('final-index.json', 'r') as ind:
+        index = json.load(ind)
+        ind.close()
+
+    with open('docID.json', 'r') as doc:
+        docID = json.load(doc)
+        doc.close()
+
+    # adds all the tf-idfs
+    with open('cs121-disk-final.json', 'r') as f:
+        doc_tfidf_sum = {}
+        for token, bit in index.items():
+            f.seek(bit)
+            j = json.loads(f.readline())
+            value = list(j.values())[0]
+            for k, v in value.items():
+                if k in doc_tfidf_sum.keys():
+                    doc_tfidf_sum[k] += math.pow(v[0], 2)
+                else:
+                    doc_tfidf_sum[k] = math.pow(v[0], 2)
+        f.close()
+
+    for k, v in doc_tfidf_sum.items():
+        doc_tfidf_sum[k] = math.sqrt(v)
+
+    # normalizes the tf-idfs in the final file
+    normalizedBitIndex = {}
+    currBit = 0
+    with open('cs121-disk-final.json', 'r') as f:
+        with open('cs121-disk-final-normalized.json', 'w+') as n:
+            for token, bit in index.items():
+                normalizedBitIndex[token] = currBit
+                f.seek(bit)
+                j = json.loads(f.readline())
+                value = list(j.values())[0]
+                for k, v in value.items():
+                    value[k][0] = v[0] / doc_tfidf_sum[k]
+                j[token] = value
+                json.dump(j, n)
+                n.write('\n')
+                currBit += len(str(j)) + 2
+            n.close()
+        f.close()
+
+    with open('final-index-normalized.json', 'w+') as f:
+        json.dump(normalizedBitIndex, f)
+        f.close()
+
+
+'''
+Creates a dictionary with cache words: to, for, the, uci, computer, or, be, and, software, science
+Return the dictionary (used in query.py)
+'''
+
+
+def writeCacheWords():
+    porterStem = PorterStemmer()
+    cacheWords = ['to', 'for', 'the', 'uci', 'computer', 'or', 'be', 'and', 'software', 'science']
+    cacheWords = [porterStem.stem(t) for t in cacheWords]
+    cache = {}
+
+    with open('cs121-disk-final-normalized.json', 'r') as n:
+        with open('final-index-normalized.json', 'r') as f:
+            index = json.load(f)
+            for word in cacheWords:
+                bit = index[word]
+                n.seek(bit)
+                j = json.loads(n.readline())
+                key = list(j.keys())[0]
+                val = list(j.values())[0]
+                cache[key] = val
+    return cache
+
+
+if __name__ == '__main__':
+    run()
---
+++ query.py
@@ -0,0 +1,330 @@
+
+import json
+import re
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+from tkinter import *
+import main
+import math
+import time
+
+'''
+Creates local GUI using TKinter and prompts user for query
+'''
+
+
+# https://stackoverflow.com/questions/36367527/centering-widgets-in-tkinter-frame-using-pack
+def search():
+    normalizedIndex = getFinalIndex('final-index-normalized.json')
+    docID = getDocID('docID.json')
+    cache = getCacheWords()
+
+    window = Tk()
+    window.title('Yet Another Search Engine')
+    window.geometry('1250x950')
+    labelTitle = Label(window, text='Search Engine')
+    labelTitle.place(relx=0.5, rely=0.2, anchor=CENTER)
+
+    frame1 = Frame(window)
+    labelResults = Label(frame1)
+    labelResults.pack(side='bottom')
+    frame1.place(relx=0.5, rely=0.65, anchor=S)
+
+    frame2 = Frame(window)
+    searchBox = Entry(frame2, width=30)
+    searchBox.pack(side='left')
+
+    def clicked():
+        start = time.perf_counter()
+        results = findResults(searchBox.get().lower(), normalizedIndex, docID, cache)
+        finish = time.perf_counter() - start
+        results += 'Time: ' + str(1000 * finish) + ' ms'
+        labelResults.configure(text=results)
+
+    search = Button(frame2, text='Search', command=clicked)
+    search.pack(side='left')
+    frame2.place(relx=0.5, rely=0.25, anchor=N)
+
+    window.mainloop()
+
+
+'''
+Processes the query and calls calculation methods for tf-idf, cos, and html tag
+weights to get the score for each document that can be shown to the user
+'''
+
+
+def findResults(text, normalizedIndex, docID, cache):
+    sets = []
+    results = ''
+    porterStem = PorterStemmer()
+
+    if len(text) == 0:
+        return 'No results found\n\n'
+
+    tokens = tokenizer(text)
+
+    tokensNoStopwords = [t for t in tokens if t not in stopwords.words('english')]
+    if len(tokensNoStopwords) != 0:
+        tokens = tokensNoStopwords
+
+    tokens = [porterStem.stem(t) for t in tokens]
+
+    queryFreq = {}
+    for t in tokens:
+        if t in queryFreq.keys():
+            queryFreq[t] += 1
+        else:
+            queryFreq[t] = 1
+
+    urls = getURLsToRank(queryFreq, normalizedIndex, 10, cache)
+
+    tf_idf_score = calculateTF_IDFSum(queryFreq, normalizedIndex, urls, cache)
+    html_score = calclateHTMLTagWeights(queryFreq, normalizedIndex, urls, cache)
+    cos_score = calculateCosine(queryFreq, normalizedIndex, urls, cache)
+
+    score = {}
+    for i in tf_idf_score.keys():
+        score[i] = 0.35 * (tf_idf_score[i]) + 0.25 * (html_score[i]) + 0.4 * (cos_score[i])
+
+    if len(score) == 0:
+        return 'No results found\n\n'
+
+    if len(score) > 10:
+        sorted_score = dict(sorted(score.items(), key=lambda x: x[1], reverse=True)[:10])
+    else:
+        sorted_score = dict(sorted(score.items(), key=lambda x: x[1], reverse=True))
+
+    results = ''
+    for k in sorted_score.keys():
+        results += docID[k] + '\n\n'
+
+    return results
+
+
+'''
+Retrieves the valid documents to calculate scoring.
+This is done by finding documents that have the most terms out of the query
+'''
+
+
+# https://stackoverflow.com/questions/26871866/print-highest-value-in-dict-with-key
+def getURLsToRank(queryFreq, normalizedIndex, k, cache):
+    tokens = {}
+    resultDict = {}
+    with open('cs121-disk-final-normalized.json', 'r') as f:
+        for t in queryFreq.keys():
+            bit = normalizedIndex[t]
+            f.seek(bit)
+            j = json.loads(f.readline())
+            docids = list(j.values())[0].keys()
+            for did in docids:
+                if did in resultDict.keys():
+                    resultDict[did] += 1
+                else:
+                    resultDict[did] = 1
+
+    if len(resultDict) > 10:
+        resultDict = dict(sorted(resultDict.items(), key=lambda x: x[1], reverse=True)[:750])
+    else:
+        resultDict = dict(sorted(resultDict.items(), key=lambda x: x[1], reverse=True))
+
+    urls = []
+    for key in resultDict.keys():
+        urls.append(key)
+
+    return urls
+
+
+'''
+Calculates the tf-idf scoring by adding all the tf-idf values
+'''
+
+
+def calculateTF_IDFSum(queryFreq, normalizedIndex, urls, cache):
+    score = {}
+    with open('cs121-disk-final-normalized.json', 'r') as f:
+        for t in queryFreq.keys():
+            if t in cache.keys():
+                value = cache[t]
+                weight = 1
+                if queryFreq[t] > 1:
+                    weight = queryFreq[t]
+                for u in urls:
+                    if u not in value.keys():
+                        break
+                    if u in score.keys():
+                        score[u] += value[u][0] * weight
+                    else:
+                        score[u] = value[u][0] * weight
+
+            elif t in normalizedIndex.keys():
+                bit = normalizedIndex[t]
+                f.seek(bit)
+                j = json.loads(f.readline())
+                value = list(j.values())[0]
+                weight = 1
+                if queryFreq[t] > 1:
+                    weight = queryFreq[t]
+                for u in urls:
+                    if u not in value.keys():
+                        break
+                    if u in score.keys():
+                        score[u] += value[u][0] * weight
+                    else:
+                        score[u] = value[u][0] * weight
+        f.close()
+    return score
+
+
+'''
+Calculates the html tag weights by adding the weights for which html
+tag the tokens were in.
+'''
+
+
+def calclateHTMLTagWeights(queryFreq, normalizedIndex, urls, cache):
+    score = {}
+    with open('cs121-disk-final-normalized.json', 'r') as f:
+        for t in queryFreq.keys():
+            if t in cache.keys():
+                value = cache[t]
+                weight = 1
+                if queryFreq[t] > 1:
+                    weight = queryFreq[t]
+                for u in urls:
+                    if u not in value.keys():
+                        break
+                    if u in score.keys():
+                        score[u] += value[u][1] * weight
+                    else:
+                        score[u] = value[u][1] * weight
+
+            elif t in normalizedIndex.keys():
+                bit = normalizedIndex[t]
+                f.seek(bit)
+                j = json.loads(f.readline())
+                value = list(j.values())[0]
+                weight = 1
+                if queryFreq[t] > 1:
+                    weight = queryFreq[t]
+                for u in urls:
+                    if u not in value.keys():
+                        break
+                    if u in score.keys():
+                        score[u] += value[u][1] * weight
+                    else:
+                        score[u] = value[u][1] * weight
+        f.close()
+    return score
+
+
+'''
+Calculates the cosine similarity between the query and relevant documents
+using tf-idf scoring
+'''
+
+
+def calculateCosine(queryFreq, normalizedIndex, urls, cache):
+    with open('cs121-disk-final-normalized.json', 'r') as f:
+        ifAllFreqOne = True
+        for k, v in queryFreq.items():
+            if v > 1:
+                ifAllFreqOne = False
+            if k in normalizedIndex.keys():
+                bit = normalizedIndex[k]
+                f.seek(bit)
+                j = json.loads(f.readline())
+                value = list(j.values())[0]
+                tf_idf = (1 + math.log(v)) * math.log(len(normalizedIndex) / len(value))
+            else:
+                tf_idf = 1 + math.log(v)
+            queryFreq[k] = tf_idf
+
+        normalize = 0
+        for k, v in queryFreq.items():
+            normalize += math.pow(v, 2)
+        normalize = math.sqrt(normalize)
+        for k, v in queryFreq.items():
+            queryFreq[k] = v / normalize
+
+        score = {}
+        for t in queryFreq.keys():
+            if t in cache.keys():
+                value = cache[t]
+                weight = 1
+                if queryFreq[t] > 1:
+                    weight = queryFreq[t]
+                for u in urls:
+                    if u not in value.keys():
+                        break
+                    if u in score.keys():
+                        score[u] += value[u][0] * queryFreq[t] * weight
+                    else:
+                        score[u] = value[u][0] * queryFreq[t] * weight
+
+            elif t in normalizedIndex.keys():
+                bit = normalizedIndex[t]
+                f.seek(bit)
+                j = json.loads(f.readline())
+                value = list(j.values())[0]
+                weight = 1
+                if queryFreq[t] > 1:
+                    weight = queryFreq[t]
+                for u in urls:
+                    if u not in value.keys():
+                        break
+                    if u in score.keys():
+                        score[u] += value[u][0] * queryFreq[t] * weight
+                    else:
+                        score[u] = value[u][0] * queryFreq[t] * weight
+
+        f.close()
+    return score
+
+
+'''
+Tokenizes the contents of the files into alphanumeric tokens
+'''
+
+
+def tokenizer(contents):
+    tokens = re.findall(r'[a-z0-9]+', contents.lower())
+    return tokens
+
+
+'''
+Retrieves the pointer index for the file with the complete index
+'''
+
+
+def getFinalIndex(file):
+    with open(file, 'r') as i:
+        index = json.load(i)
+        i.close()
+    return index
+
+
+'''
+Retrieves the document IDs for the index
+'''
+
+
+def getDocID(file):
+    with open(file, 'r') as d:
+        docID = json.load(d)
+        d.close()
+    return docID
+
+
+'''
+Retrieves cache words by calling function in main.py
+'''
+
+
+def getCacheWords():
+    return main.writeCacheWords()
+
+
+if __name__ == '__main__':
+    search()
